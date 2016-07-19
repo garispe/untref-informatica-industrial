2e7e589198a7ad9f993eef77563e85a17665157b
garispe <guillermo.arispe@gmail.com>
2016-07-18 22:00:13 -0300

    Crecion de proyecto

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,326 @@
+﻿namespace Microsoft.Samples.Kinect.DepthBasics
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+    using System.Drawing;
+    using System.Windows.Threading;
+    public partial class MainWindow : Window
+    {
+
+        private KinectSensor sensor;
+        private WriteableBitmap colorBitmap;
+        private byte[] pixelesColores;
+        private BitmapEncoder encoder;
+        private string path;
+        private string misFotos;
+        private string hora;
+        private ProcesadorDeImagen procesador;
+        private BitmapImage imagenConFiltro;
+        private DispatcherTimer timer;
+        private DepthImagePixel[] pixelesProfundidad;
+
+        private int numero = 1;
+        private bool visible = false;
+        private bool iniciado = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            procesador = new ProcesadorDeImagen();
+            misFotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\industrial";
+
+            hora = DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+        }
+
+        private void iniciarVentana(object sender, RoutedEventArgs e)
+        {
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                this.pixelesColores = new byte[this.sensor.ColorStream.FramePixelDataLength];
+
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth,
+                    this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                this.Video.Source = this.colorBitmap;
+
+                this.sensor.ColorFrameReady += this.colorFrameListener;
+
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+        }
+
+        private void cerrarVentana(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        private void colorFrameListener(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame frameColores = e.OpenColorImageFrame())
+            {
+                if (frameColores != null)
+                {
+                    frameColores.CopyPixelDataTo(this.pixelesColores);
+
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.pixelesColores,
+                        this.colorBitmap.PixelWidth * sizeof(int), 0);
+                }
+            }
+        }
+
+        private void profundidadFrameListener(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame frameProfundidad = e.OpenDepthImageFrame())
+            {
+                if (frameProfundidad != null)
+                {
+                    frameProfundidad.CopyDepthImagePixelDataTo(this.pixelesProfundidad);
+
+                    int min = frameProfundidad.MinDepth;
+                    int max = frameProfundidad.MaxDepth;
+
+                    int colorPixelIndex = 0;
+
+                    for (int i = 0; i < this.pixelesProfundidad.Length; ++i)
+                    {
+
+                        short profundidad = pixelesProfundidad[i].Depth;
+
+                        byte intensidad = (byte)(profundidad >= min && profundidad <= max ? profundidad : 0);
+
+                        this.pixelesColores[colorPixelIndex++] = intensidad;
+                        this.pixelesColores[colorPixelIndex++] = intensidad;
+                        this.pixelesColores[colorPixelIndex++] = intensidad;
+
+                        ++colorPixelIndex;
+                    }
+
+                    this.colorBitmap.WritePixels(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.pixelesColores, this.colorBitmap.PixelWidth * sizeof(int), 0);
+                }
+            }
+        }
+
+        private void listenerScreenshot(object sender, RoutedEventArgs e)
+        {
+            this.encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(this.colorBitmap));
+            hora = DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+            path = Path.Combine(misFotos, "Screenshot" + hora + ".bmp");
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create))
+                {
+                    encoder.Save(fs);
+                }
+                this.Imagen.Source = new BitmapImage(new Uri(path));
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("La ruta elegida no es una ruta disponible");
+            }
+        }
+
+        private void listenerFiltroCanny(object sender, RoutedEventArgs e)
+        {
+            if (this.imagenConFiltro != null)
+            {
+                Bitmap bitmap = this.procesador.bitmapFromBitmapImage(this.imagenConFiltro);
+                this.imagenConFiltro = this.procesador.aplicarFiltroCanny(bitmap, misFotos, hora);
+                this.Imagen.Source = this.imagenConFiltro;
+            }
+            else
+            {
+                MessageBox.Show("Primero debe aplicar un efecto");
+            }
+        }
+
+        private void listenerFiltroSobel(object sender, RoutedEventArgs e)
+        {
+            if (this.imagenConFiltro != null)
+            {
+                Bitmap bitmap = this.procesador.bitmapFromBitmapImage(this.imagenConFiltro);
+                this.imagenConFiltro = this.procesador.aplicarFiltroSobel(bitmap, misFotos, hora);
+                this.Imagen.Source = this.imagenConFiltro;
+            }
+            else
+            {
+                MessageBox.Show("Primero debe aplicar un efecto");
+            }
+        }
+
+        private void listenerDetectarFormas(object sender, RoutedEventArgs e)
+        {
+
+            if (this.imagenConFiltro != null)
+            {
+                Bitmap bitmap = this.procesador.bitmapFromBitmapImage(this.imagenConFiltro);
+                this.Imagen.Source = this.procesador.detectarCuadrilateros(bitmap, misFotos, hora);
+            }
+            else
+            {
+                MessageBox.Show("Debes tener una imagen con un filtro aplicado");
+            }
+        }
+
+        private void iniciarTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(2000);
+            timer.Tick += eventoARealizar;
+            timer.Start();
+        }
+
+        private void eventoARealizar(object source, EventArgs e)
+        {
+            path = Path.Combine(misFotos, "timer/Foto" + numero + ".bmp");
+            BitmapImage bitmapImageTimer = new BitmapImage(new Uri(path));
+            Console.WriteLine("paso");
+            this.Imagen.Source = bitmapImageTimer;
+
+            if (numero == 4)
+            {
+                timer.Stop();
+            }
+            numero++;
+        }
+
+        private void listenerPanelControl(object sender, RoutedEventArgs e)
+        {
+            if (!visible)
+            {
+                this.iniciarProcesamiento.Visibility = Visibility.Visible;
+                this.comboBoxEfectos.Visibility = Visibility.Hidden;
+                this.filtroCanny.Visibility = Visibility.Hidden;
+                this.filtroSobel.Visibility = Visibility.Hidden;
+                this.detectorCuadrilateros.Visibility = Visibility.Hidden;
+                this.screenShot.Visibility = Visibility.Hidden;
+                visible = true;
+            }
+            else
+            {
+                this.iniciarProcesamiento.Visibility = Visibility.Hidden;
+                this.comboBoxEfectos.Visibility = Visibility.Visible;
+                this.filtroCanny.Visibility = Visibility.Visible;
+                this.filtroSobel.Visibility = Visibility.Visible;
+                this.detectorCuadrilateros.Visibility = Visibility.Visible;
+                this.screenShot.Visibility = Visibility.Visible;
+                visible = false;
+            }
+        }
+
+        private void listenerIniciarProcesamiento(object sender, RoutedEventArgs e)
+        {
+            if (iniciado)
+            {
+                this.iniciarProcesamiento.Content = "Iniciar procesamiento";
+                iniciado = false;
+                timer.Stop();
+            }
+            else
+            {
+                this.iniciarProcesamiento.Content = "Parar procesamiento";
+                iniciado = true;
+                iniciarTimer();
+            }
+        }
+
+        private void comboBoxEfectos_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            path = Path.Combine(misFotos, "Screenshot11-36-23.bmp");
+            int efecto = comboBoxEfectos.SelectedIndex;
+
+            switch (efecto)
+            {
+                case 0:
+                    this.imagenConFiltro = this.procesador.Bitonal(path, misFotos, hora, System.Drawing.Color.Black, System.Drawing.Color.Green, 250);
+                    this.Imagen.Source = this.imagenConFiltro;
+                    break;
+
+                case 1:
+                    this.imagenConFiltro = this.procesador.gamma(path, misFotos, hora, 3, 3, 3);
+                    this.Imagen.Source = this.imagenConFiltro;
+                    break;
+
+                case 2:
+                    this.imagenConFiltro = this.procesador.aplicarGrises(path, misFotos, hora);
+                    this.Imagen.Source = this.imagenConFiltro;
+                    break;
+
+                case 3:
+                    this.imagenConFiltro = this.procesador.aplicarContraste(path, misFotos, hora, 30);
+                    this.Imagen.Source = this.imagenConFiltro;
+                    break;
+
+                    /**
+                    case :
+                        this.imagenConFiltro = this.procesador.aplicarHough(path, misFotos);
+                        this.Imagen.Source = this.imagenConFiltro;
+                        break;
+                    */
+            }
+        }
+
+        private void profundidad_Checked(object sender, RoutedEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                if (this.profundidad.IsChecked == false)
+                {
+                    this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                    this.pixelesColores = new byte[this.sensor.ColorStream.FramePixelDataLength];
+
+                    this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth,
+                        this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                    this.Video.Source = this.colorBitmap;
+
+                    this.sensor.ColorFrameReady += this.colorFrameListener;
+                }
+                else
+                {
+                    this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+
+                    this.pixelesProfundidad = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+                    this.pixelesColores = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+                    this.colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                    this.Video.Source = this.colorBitmap;
+
+                    this.sensor.DepthFrameReady += this.profundidadFrameListener;
+                }
+            }
+        }
+    }
+}
---
+++ ProcesadorDeImagen.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using AForge.Imaging.Filters;
+using System.Drawing;
+using AForge.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+using AForge;
+using AForge.Math.Geometry;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Samples.Kinect.DepthBasics
+{
+    class ProcesadorDeImagen
+    {
+        // Canny
+        public BitmapImage aplicarFiltroCanny(Bitmap bitmap, string misFotos, string hora)
+        {
+            Rectangle rectangulo = new Rectangle(0, 0, 640, 480);
+            var foto = bitmap.Clone(rectangulo, PixelFormat.Format8bppIndexed);
+
+            UnmanagedImage uI = UnmanagedImage.FromManagedImage(foto);
+
+            CannyEdgeDetector filtroBordes = new CannyEdgeDetector();
+            UnmanagedImage fotoConFiltro = filtroBordes.Apply(uI);
+            Bitmap bitmapConFiltro = fotoConFiltro.ToManagedImage();
+
+            string urlFotoFiltro = Path.Combine(misFotos, "Canny.bmp");
+
+            bitmapConFiltro.Save(urlFotoFiltro);
+
+            return new BitmapImage(new Uri(urlFotoFiltro));
+        }
+
+        // Sobel
+        public BitmapImage aplicarFiltroSobel(Bitmap bitmap, string misFotos, string hora)
+        {
+            Rectangle rectangulo = new Rectangle(0, 0, 640, 480);
+            var foto = bitmap.Clone(rectangulo, PixelFormat.Format8bppIndexed);
+
+            UnmanagedImage uI = UnmanagedImage.FromManagedImage(foto);
+
+            SobelEdgeDetector filtroBordes = new SobelEdgeDetector();
+            UnmanagedImage fotoConFiltro = filtroBordes.Apply(uI);
+            Bitmap bitmapConFiltro = fotoConFiltro.ToManagedImage();
+
+            string urlFotoFiltro = Path.Combine(misFotos, "Sobel.bmp");
+
+            bitmapConFiltro.Save(urlFotoFiltro);
+
+            return new BitmapImage(new Uri(urlFotoFiltro));
+        }
+
+        // Detector de cuadrilateros
+        public BitmapImage detectarCuadrilateros(Bitmap bitmap, string misFotos, string hora)
+        {
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.ProcessImage(bitmap);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            Graphics g = Graphics.FromImage(bitmap);
+            Pen bluePen = new Pen(Color.Blue, 2);
+            Pen redPen = new Pen(Color.Red, 2);
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+                List<IntPoint> corners = PointsCloud.FindQuadrilateralCorners(edgePoints);
+
+                SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+                if (shapeChecker.IsQuadrilateral(edgePoints, out corners))
+                {
+                    g.DrawPolygon(redPen, toPointsArray(corners));
+                }
+                else
+                {
+                    g.DrawPolygon(bluePen, toPointsArray(corners));
+
+                }
+            }
+
+            bluePen.Dispose();
+            g.Dispose();
+
+            string urlConFormas = Path.Combine(misFotos, "ImagenConFormas.bmp");
+
+            bitmap.Save(urlConFormas);
+
+            return new BitmapImage(new Uri(urlConFormas));
+        }
+
+        private System.Drawing.Point[] toPointsArray(List<IntPoint> points)
+        {
+            System.Drawing.Point[] array = new System.Drawing.Point[points.Count];
+
+            for (int i = 0, n = points.Count; i < n; i++)
+            {
+                array[i] = new System.Drawing.Point(points[i].X, points[i].Y);
+            }
+
+            return array;
+        }
+
+        // Transformar bitmap en BitmapImage
+        public Bitmap bitmapFromBitmapImage(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStraeam = new MemoryStream())
+            {
+                BitmapEncoder encoder = new BmpBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+                encoder.Save(outStraeam);
+                Bitmap bitmap = new Bitmap(outStraeam);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        // Contraste
+        public BitmapImage aplicarContraste(string path, string misFotos, string hora, int threshold)
+        {
+            Bitmap sourceBitmap = new Bitmap(path);
+            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
+                                        sourceBitmap.Width, sourceBitmap.Height),
+                                        ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            sourceBitmap.UnlockBits(sourceData);
+            double contrastLevel = Math.Pow((100.0 + threshold) / 100.0, 2);
+
+            double azul = 0;
+            double verde = 0;
+            double rojo = 0;
+
+            for (int k = 0; k + 4 < pixelBuffer.Length; k += 4) {
+                azul = ((((pixelBuffer[k] / 255.0) - 0.5) *
+                            contrastLevel) + 0.5) * 255.0;
+                verde = ((((pixelBuffer[k + 1] / 255.0) - 0.5) *
+                            contrastLevel) + 0.5) * 255.0;
+                rojo = ((((pixelBuffer[k + 2] / 255.0) - 0.5) *
+                            contrastLevel) + 0.5) * 255.0;
+
+                if (azul > 255) {
+                    azul = 255;
+                } else if (azul < 0) {
+                    azul = 0;
+                }
+                if (verde > 255) {
+                    verde = 255;
+                } else if (verde < 0) {
+                    verde = 0;
+                }
+                if (rojo > 255) {
+                    rojo = 255;
+                } else if (rojo < 0) {
+                    rojo = 0;
+                }
+                pixelBuffer[k] = (byte)azul;
+                pixelBuffer[k + 1] = (byte)verde;
+                pixelBuffer[k + 2] = (byte)rojo;
+            }
+
+            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0,
+                                        resultBitmap.Width, resultBitmap.Height),
+                                        ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(pixelBuffer, 0, resultData.Scan0, pixelBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+
+            string urlConFormas = Path.Combine(misFotos, "Contraste.bmp");
+
+            resultBitmap.Save(urlConFormas);
+
+            return new BitmapImage(new Uri(urlConFormas));
+        }
+
+        // Bitonal
+        public BitmapImage Bitonal(string path, string misFotos, string hora, 
+            Color darkColor, Color lightColor, int threshold) {
+
+            Bitmap sourceBitmap = new Bitmap(path);
+            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0, sourceBitmap.Width, 
+                sourceBitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            sourceBitmap.UnlockBits(sourceData);
+            
+            for (int k = 0; k + 4 < pixelBuffer.Length; k += 4) {
+
+                if (pixelBuffer[k] + pixelBuffer[k + 1] + pixelBuffer[k + 2] <= threshold) {
+                    pixelBuffer[k] = darkColor.B;
+                    pixelBuffer[k + 1] = darkColor.G;
+                    pixelBuffer[k + 2] = darkColor.R;
+                } else {
+                    pixelBuffer[k] = lightColor.B;
+                    pixelBuffer[k + 1] = lightColor.G;
+                    pixelBuffer[k + 2] = lightColor.R;
+                }
+            }
+            
+            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height),
+                                    ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            
+            Marshal.Copy(pixelBuffer, 0, resultData.Scan0, pixelBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+
+            string urlConFormas = Path.Combine(misFotos, "Bitonal.bmp");
+
+            resultBitmap.Save(urlConFormas);
+
+            return new BitmapImage(new Uri(urlConFormas));
+        }
+
+        // Gamma
+        private byte[] CreateGammaArray(double color)
+        {
+            byte[] gammaArray = new byte[256];
+            for (int i = 0; i < 256; ++i) {
+                gammaArray[i] = (byte)Math.Min(255,
+                    (int)((255.0 * Math.Pow(i / 255.0, 1.0 / color)) + 0.5));
+            }
+            return gammaArray;
+        }
+
+        public BitmapImage gamma(string path, string misFotos, string hora
+            , double red, double green, double blue)
+        {
+            Bitmap bitmap = new Bitmap(path);
+            Bitmap temp = (Bitmap)bitmap;
+            Bitmap bmap = (Bitmap)temp.Clone();
+            Color c;
+            byte[] redGamma = CreateGammaArray(red);
+            byte[] greenGamma = CreateGammaArray(green);
+            byte[] blueGamma = CreateGammaArray(blue);
+            for (int i = 0; i < bmap.Width; i++) {
+                for (int j = 0; j < bmap.Height; j++)
+                {
+                    c = bmap.GetPixel(i, j);
+                    bmap.SetPixel(i, j, Color.FromArgb(redGamma[c.R],
+                       greenGamma[c.G], blueGamma[c.B]));
+                }
+            }
+            bitmap = (Bitmap)bmap.Clone();
+
+            string urlConFormas = Path.Combine(misFotos, "Gamma.bmp");
+
+            bitmap.Save(urlConFormas);
+
+            return new BitmapImage(new Uri(urlConFormas));
+        }
+
+        // Escala gris
+        public BitmapImage aplicarGrises(string path, string misFotos, string hora)
+        {
+
+            Bitmap bitmap = new Bitmap(path);
+            Bitmap temp = bitmap;
+            Bitmap bmap = (Bitmap)temp.Clone();
+            Color c;
+            for (int i = 0; i < bmap.Width; i++)
+            {
+                for (int j = 0; j < bmap.Height; j++)
+                {
+                    c = bmap.GetPixel(i, j);
+                    byte gray = (byte)(.299 * c.R + .587 * c.G + .114 * c.B);
+
+                    bmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                }
+            }
+            bitmap = (Bitmap)bmap.Clone();
+
+            string urlConFormas = Path.Combine(misFotos, "Grises.bmp");
+
+            bitmap.Save(urlConFormas);
+
+            return new BitmapImage(new Uri(urlConFormas));
+        }
+
+        // Hough
+        public BitmapImage aplicarHough(string path, string misFotos)
+        {
+            Bitmap bitmap = new Bitmap(path);
+            HoughLineTransformation lineTransform = new HoughLineTransformation();
+            // apply Hough line transofrm
+            lineTransform.ProcessImage(bitmap);
+            Bitmap houghLineImage = lineTransform.ToBitmap();
+            // get lines using relative intensity
+            HoughLine[] lines = lineTransform.GetLinesByRelativeIntensity(0.6);
+
+            foreach (HoughLine line in lines)
+            {
+                // get line's radius and theta values
+                int r = line.Radius;
+                double t = line.Theta;
+                
+                // check if line is in lower part of the image
+                if (r < 0)
+                {
+                    t += 180;
+                    r = -r;
+                }
+
+                // convert degrees to radians
+                t = (t / 180) * Math.PI;
+
+                // get image centers (all coordinate are measured relative
+                // to center)
+                int w2 = bitmap.Width / 2;
+                int h2 = bitmap.Height / 2;
+
+                double x0 = 0, x1 = 0, y0 = 0, y1 = 0;
+
+                if (line.Theta != 0)
+                {
+                    // none-vertical line
+                    x0 = -w2; // most left point
+                    x1 = w2;  // most right point
+
+                    // calculate corresponding y values
+                    y0 = (-Math.Cos(t) * x0 + r) / Math.Sin(t);
+                    y1 = (-Math.Cos(t) * x1 + r) / Math.Sin(t);
+                }
+                else
+                {
+                    // vertical line
+                    x0 = line.Radius;
+                    x1 = line.Radius;
+
+                    y0 = h2;
+                    y1 = -h2;
+                }
+
+                // draw line on the image
+                BitmapData bmp = new BitmapData();
+
+                Rectangle rec = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                BitmapData bmpData = bitmap.LockBits(rec, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+                bitmap.UnlockBits(bmpData);
+
+                Drawing.Line(bmpData,
+                    new IntPoint((int)x0 + w2, h2 - (int)y0),
+                    new IntPoint((int)x1 + w2, h2 - (int)y1),
+                    Color.White);
+            }
+
+            string urlConFormas = Path.Combine(misFotos, "Hough.bmp");
+
+            bitmap.Save(urlConFormas);
+
+            return new BitmapImage(new Uri(urlConFormas));
+        }
+
+    }
+}
